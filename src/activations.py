05c5d84c56092fe0b9cd98e745a63f6648cd5b75
JeonYeon-Hwang <ubitnoa@gmail.com>
2026-05-23 00:31:38 +0900

    feat: ReLU forward 구현 + 테스트

--- src/activations.py
+++ src/activations.py
@@ -17,6 +17,9 @@ class ReLU:
     은닉층에서 음수 값은 0으로 막고, 양수 값은 그대로 통과시킵니다.
     forward에서 만든 mask는 backward 때 "어느 위치로 gradient를 흘릴지" 결정하는 데 사용됩니다.
     """
+    def __init__(self):
+        self.mask = None;
+
 
     def forward(self, x):
         """
@@ -26,7 +29,12 @@ class ReLU:
         Returns:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
-        # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
+        self.mask = (x <= 0) 
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
         raise NotImplementedError("ReLU.forward를 구현하세요.")
 
     def backward(self, dout):
