d759fa3010bf00fcdffcf1194d2d64e8337fe27f
JeonYeon-Hwang <ubitnoa@gmail.com>
2026-05-24 22:50:12 +0900

    feat: affine 까지 구현 완료 + 패스

--- src/layers.py
+++ src/layers.py
@@ -23,6 +23,10 @@ class Affine:
         """가중치 W와 편향 b를 외부 params dict와 같은 배열 객체로 공유합니다."""
         self.W = W
         self.b = b
+        
+        self.x = None
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
         """
@@ -32,7 +36,11 @@ class Affine:
         Returns:
             (batch_size, output_dim)
         """
-        # TODO: backward에서 사용할 입력 x를 저장하고 x @ W + b를 반환하세요.
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        
+        return out
+    
         raise NotImplementedError("Affine.forward를 구현하세요.")
 
     def backward(self, dout):
@@ -46,8 +54,12 @@ class Affine:
         Side effects:
             self.dW, self.db에 optimizer가 사용할 gradient를 저장합니다.
         """
-        # TODO: self.dW, self.db, dx를 계산하세요.
-        # 힌트: dW = x.T @ dout, db = batch 방향 합, dx = dout @ W.T
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = np.dot(dout, self.W.T)
+
+        return dx
+
         raise NotImplementedError("Affine.backward를 구현하세요.")
 
 
